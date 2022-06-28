90cf66ef8dc558232732fdfa75389d53fb2f0f8b
amIsachin <amIsachin@hotmail.com>
2022-06-28 16:17:35 +0530

    Add one record in University student as well as course table

--- University.sql
+++ University.sql
@@ -17,4 +17,12 @@ CREATE TABLE Course
 	[Name] VARCHAR(255),
 	CreatedOn DATE,
 	Student__ID INT FOREIGN KEY REFERENCES University__Student(Enrolment__Number)
+	ON UPDATE CASCADE
+	ON DELETE CASCADE
 );
+
+INSERT INTO University__Student VALUES ('Sachin', 23,'Male',1200,'Delhi','Document','Image',GETDATE())
+INSERT INTO Course VALUES ('c-sharp', GETDATE(), 10000)
+
+SELECT * FROM University__Student
+SELECT * FROM Course
